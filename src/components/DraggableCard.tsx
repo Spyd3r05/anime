b0df2970aedc2506dfe88987f27534cd2e212aae
Spyd3r05 <agnesnzivu05@gmail.com>
2026-04-09 18:09:29 +0300

    feat: implement single card drag and animation logic

---
+++ src/components/DraggableCard.tsx
@@ -0,0 +1,28 @@
+import { motion } from "motion/react";
+import { useCardRotation } from "../hook/useCardRotation";
+
+//props interface
+interface DraggableCardProps{
+  onSendBack?: () =>void;
+  children: React.ReactNode;
+  className?:string;  
+}
+const DraggableCard = ({onSendBack, children, className} : DraggableCardProps )=> {
+  const {x,y, rotateX, rotateY, handleDrag} = useCardRotation(onSendBack);
+
+  return (
+    <motion.div
+    className={className}
+    style={{x,y,rotateY,rotateX}}
+    drag
+    dragConstraints={{top:0, right:0, left:0, bottom:0}}
+    dragElastic={0.6}
+    onDragEnd={handleDrag}
+    whileTap={{cursor: "grabbing"}}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default DraggableCard
